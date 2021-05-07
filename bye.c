431f659787340d64d034e296addff592f784c536
GovindVijay <74721016+GovindVijay@users.noreply.github.com>
2021-05-07 17:01:24 +0530

    Add files via upload

---
+++ bye.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("hello");
+    printf("\nEnter the number:");
+    scanf("%d",&a);
+    printf("The number you entered is:%d",a);
+    
+}
---
+++ odd_even_checker.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    //this program is to check whether the number is odd or even
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("this number is even\n");
+    else
+    printf("this number is odd\n");
+}
