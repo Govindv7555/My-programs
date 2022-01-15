1f3b85a2fed5bb0e4f884acf067db8999419acfb
GovindVijay <74721016+GovindVijay@users.noreply.github.com>
2022-01-15 17:41:20 +0530

    Add files via upload

---
+++ ATM.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+  int b,c,d;
+  char a[20];
+  b=3000;
+  printf("Welcome,to Govind's ATM\n");
+  printf("Enter your Name =\n");
+  scanf("%s",&a);
+  printf("hello,%s\nYour current balance is = %d\n",a,b);
+  printf("Press 1 to Debit\n");
+  scanf("%d",&c);
+  if(c==1)
+ {
+     if(c<b){
+     printf("Enter the amount you want to debit = \n");
+     scanf("%d",&d);
+     printf("Successfully Debited\nThe Current Balance is = %d\n",b-d);
+     printf("Thanks for using Govind's ATM\n");
+     }
+     else
+     {
+         printf("Entered incorrect amount to debit\n");
+     }
+ }
+ else
+ {
+     printf("Sorry not in the list\n");
+ }
+
+
+
+
+}
